9b376e64419fc22d218ab7e221db1e85efa50ec0
Dr. ERDI Gergo <gergo@erdi.hu>
2015-01-23 08:49:50 +0800

    Update to Elm 0.13

--- Audio.elm
+++ Audio.elm
@@ -6,16 +6,17 @@ import Native.Audio
 import Signal
 import Keyboard
 import Set
+import Time (Time)
 
 {-| An Action controls how audio is heard. -}
-data Action = Play | Pause | Seek Time | NoChange
+type Action = Play | Pause | Seek Time | NoChange
 
 {-| A Properties record contains information related to audio -}
-type Properties = { duration : Time, currentTime : Time, ended : Bool }
+type alias Properties = { duration : Time, currentTime : Time, ended : Bool }
 
 {-| A Triggers record describes on what events you would like to receive
     a Properties record -}
-type Triggers = { timeupdate : Bool, ended : Bool }
+type alias Triggers = { timeupdate : Bool, ended : Bool }
 
 {-| The defaultTriggers is a record for ease of use. All Triggers are
     set to False. -}
@@ -25,7 +26,7 @@ defaultTriggers = { timeupdate = False, ended = False }
 {-| An Event is used to describe why the Signal returned by the
     audio function was fired.
  -}
-data Event = TimeUpdate
+type Event = TimeUpdate
            | Ended
            | Created
 
@@ -34,10 +35,10 @@ data Event = TimeUpdate
     triggers - A Triggers Record describing on which events we want to receive Properties
     propertiesHandler - A Function that is called each time Properties are calculated.
     actions - A Signal of incomming actions that manipulate the audio -}
-type Builder = { src : String, 
-                 triggers : Triggers, 
-                 propertiesHandler : (Properties -> Maybe Action),
-                 actions : Signal Action }
+type alias Builder = { src : String,
+                       triggers : Triggers,
+                       propertiesHandler : (Properties -> Maybe Action),
+                       actions : Signal Action }
 
 {-| Given a Builder, creates an Signal that fires on the specified events
     and produces the properties during those events.
